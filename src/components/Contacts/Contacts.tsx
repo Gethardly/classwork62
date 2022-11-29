265d64e874416b8d662db8ab9a125ffc762cd859
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2022-11-29 18:03:02 +0600

    Separate component contacts

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <div>
+      <p>E-mail: <a>gethardlyever@gmail.com</a></p>
+      <p>Number: +996708552267</p>
+    </div>
+  );
+};
+
+export default Contacts;
