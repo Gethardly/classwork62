b82a20a99cfde80ce6f64c599072b191692a2d4b
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2022-11-29 18:00:56 +0600

    Basic files of react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar/NavBar";
+import {Link, Route, Routes, Outlet, NavLink, useNavigate} from "react-router-dom";
+import Plovo from "./containers/Plovo/Plovo";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Contacts from "./components/Contacts/Contacts";
+import Home from "./containers/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header><NavBar/></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+
+          <Route path="/about-us" element={<AboutUs/>}>
+            <Route path="contacts" element={<Contacts/>}/>
+          </Route>
+
+          <Route path="/portfolio" element={(<><Outlet/><Link to="plovo">Plovo</Link></>)}>
+            <Route path="plovo" element={(<Plovo/>)}>
+            </Route>
+          </Route>
+
+
+          <Route path="*" element={<h1>Not found ;(</h1>}/>
+        </Routes>
+      </main>
+    </>
   );
 }
 
